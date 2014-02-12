eb8f31df3815895c7c9d651c1db50e053550a6c8
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-02-12 17:46:15 +0400

    * добавил тестовый файл для скриплета (на PoSh)

---
+++ ITG.BDE_PRO/Wrappers/BDETest.ps1
@@ -0,0 +1,22 @@
+﻿$t = New-Object -ComObject ITG.BDEAdministrator;
+$t.Version;
+
+$t.Init( $null );
+
+# $t.ImportODBC( $null );
+$t.AddAlias( 'NevaTest3303P', 'Microsoft dBase Driver (*.dbf)', 'DSN:NevaTest3303P;DESCRIPTION:База данных результатов поверок НеваТест 3303П', $null );
+# $t.DeleteAlias( 'NevaTest3303P', $null );
+$t.SaveConfigFile( $null );
+
+$t.Done( $null );
+
+$t.Init( $null );
+
+# $t.ImportODBC( $null );
+# $t.AddAlias( 'NevaTest3303P', 'Microsoft dBase Driver (*.dbf)', 'DSN:NevaTest3303P;DESCRIPTION:База данных результатов поверок НеваТест 3303П', $null );
+$t.DeleteAlias( 'NevaTest3303P', $null );
+$t.SaveConfigFile( $null );
+
+$t.Done( $null );
+
+[System.Runtime.InteropServices.Marshal]::ReleaseComObject( $t );
